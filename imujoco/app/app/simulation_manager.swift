30f97b5fe201ce1a59b342994571f705d2ae7e4f
hhkb.logi <hhkb.logi@gmail.com>
2026-02-20 11:19:12 +0000

    Revert "tmp: break Simple Pendulum resource name to test load error path"
    This reverts commit 52df44975b23e4495f96edada21e59fde5975864.

--- imujoco/app/app/simulation_manager.swift
+++ imujoco/app/app/simulation_manager.swift
@@ -392,7 +392,7 @@ final class SimulationGridManager: @unchecked Sendable {
                          keyframe: nil, timestep: nil, cameraElevation: nil, cameraAzimuth: nil, cameraDistance: nil),
             BundledModel(name: "Humanoid (Supine)", source: .mujoco, resource: "humanoid_supine", subdirectory: nil,
                          keyframe: "supine", timestep: nil, cameraElevation: nil, cameraAzimuth: nil, cameraDistance: nil),
-            BundledModel(name: "Simple Pendulum", source: .imujoco, resource: "simple_pendulum_DOES_NOT_EXIST", subdirectory: nil,
+            BundledModel(name: "Simple Pendulum", source: .imujoco, resource: "simple_pendulum", subdirectory: nil,
                          keyframe: "start", timestep: nil, cameraElevation: nil, cameraAzimuth: nil, cameraDistance: nil),
             BundledModel(name: "Agility Cassie", source: .menagerie, resource: "scene", subdirectory: "agility_cassie",
                          keyframe: "home", timestep: nil, cameraElevation: nil, cameraAzimuth: nil, cameraDistance: nil),
